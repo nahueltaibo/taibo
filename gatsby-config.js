7c27c944f9452e30ae7c3035da54f9711774b342
Nahuel Taibo <nahueltaibo@gmail.com>
2023-02-25 01:24:51 -0500

    Building new from latest packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,40 @@
-const siteName = "Taibo";
-const siteShortName = "Taibo";
-const siteUrl = "https://taibo.ca";
-const siteDescription = "My adventures as a Maker and Software developer";
-const siteKeyword = "";
-const siteLogo = "logo.png";
-const siteLogoFolder = `static/${siteLogo}`;
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-});
-
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
   siteMetadata: {
-    title: siteName,
-    siteUrl,
-    description: siteDescription,
+    title: `Gatsby Starter Blog`,
+    author: {
+      name: `Kyle Mathews`,
+      summary: `who lives and works in San Francisco building useful things.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    social: {
+      twitter: `kylemathews`,
+    },
   },
   plugins: [
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-robots-txt",
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-image`,
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content`,
-        name: "contents",
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "images",
-        path: `${__dirname}/static`,
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     {
@@ -44,78 +44,83 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 2048,
-              linkImagesToOriginal: true,
+              maxWidth: 630,
             },
           },
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
           },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
           {
-            resolve: `gatsby-remark-copy-linked-files`,
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `{
+              allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+                nodes {
+                  excerpt
+                  html
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                    date
+                  }
+                }
+              }
+            }`,
+            output: "/rss.xml",
+            title: "Gatsby Starter Blog RSS Feed",
           },
         ],
       },
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: siteName,
-        short_name: siteShortName,
-        description: siteDescription,
+        name: `Gatsby Starter Blog`,
+        short_name: `Gatsby`,
+        start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#ffffff`,
-        display: `standalone`,
-        lang: "en",
-        start_url: "/",
-        include_favicon: true,
-        icon: siteLogoFolder, // This path is relative to the root of the site.
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    {
-      resolve: "gatsby-plugin-seo",
-      options: {
-        siteName,
-        defaultSiteImage: siteLogo,
-        siteUrl,
-        keywords: siteKeyword,
-        globalSchema: `{
-            "@type": "WebSite",
-            "@id": "${siteUrl}",
-            "url": "${siteUrl}",
-            "name": "${siteName}",
-            "publisher": {
-              "@id": "${siteUrl}"
-            },
-            "image": {
-              "@type": "ImageObject",
-              "@id": "${siteUrl}${siteLogo}",
-              "url": "${siteUrl}${siteLogo}",
-              "caption": siteName
-            }
-          }`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
-      options: {
-        siteUrl,
-        noTrailingSlash: true,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-    },
-    {
-      resolve: `gatsby-plugin-slug`,
-    },
-    {
-      resolve: `gatsby-plugin-disqus`,
-      options: {
-        shortname: process.env.GATSBY_DISQUS_NAME
-      }
     },
   ],
-};
+}
